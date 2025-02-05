b5244d08828b49ddbf3249c44bf108d5e54e2f28
Gabby-Tech1 <gkaquainoo@gmail.com>
2025-02-05 15:48:50 +0000

    Completed list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,7 +1,14 @@
 if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
     n = int(input())
-    arr = map(int, input().split())
+
+    # List comprehension to generate valid coordinates
+    result = [[i, j, k] 
+              for i in range(x + 1) 
+              for j in range(y + 1) 
+              for k in range(z + 1) 
+              if i + j + k != n]
     
-    result = set(arr)
-    result.remove(max(result))
-    print(max(result))
+    print(result)
